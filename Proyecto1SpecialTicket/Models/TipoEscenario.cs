a219881ea07de0496d7fdc15ead609422947444f
Derian Rodriguez <dmrodriguez2000@gmail.com>
2023-03-12 17:02:04 -0600

    Update of the base styles of the last 4 views

--- Proyecto1SpecialTicket/Models/TipoEscenario.cs
+++ Proyecto1SpecialTicket/Models/TipoEscenario.cs
@@ -1,25 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Proyecto1SpecialTicket.Models;
 
 public partial class TipoEscenario
 {
+    [DisplayName("Id")]
     public int Id { get; set; }
 
+    [DisplayName("Descripción")]
     public string Descripcion { get; set; } = null!;
 
+    [DisplayName("Fecha de creación")]
     public DateTime CreatedAt { get; set; }
 
+    [DisplayName("Creado por")]
     public int CreatedBy { get; set; }
 
+    [DisplayName("Fecha de actualización")]
     public DateTime UpdatedAt { get; set; }
 
+    [DisplayName("Actualizado por")]
     public int UpdatedBy { get; set; }
 
+    [DisplayName("Activo")]
     public bool Active { get; set; }
 
+    [DisplayName("Id escenario")]
     public int IdEscenario { get; set; }
 
+    [DisplayName("Id escenario")]
     public virtual Escenario IdEscenarioNavigation { get; set; } = null!;
 }
--- Proyecto1SpecialTicket/Models/TipoEvento.cs
+++ Proyecto1SpecialTicket/Models/TipoEvento.cs
@@ -1,22 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Proyecto1SpecialTicket.Models;
 
 public partial class TipoEvento
 {
+    [DisplayName("Id")]
     public int Id { get; set; }
 
+    [DisplayName("Descripción")]
     public string Descripcion { get; set; } = null!;
 
+    [DisplayName("Fecha de creación")]
     public DateTime CreatedAt { get; set; }
 
+    [DisplayName("Creado por")]
     public int CreatedBy { get; set; }
 
+    [DisplayName("Fecha de actualización")]
     public DateTime UpdatedAt { get; set; }
 
+    [DisplayName("Actualizado por")]
     public int UpdatedBy { get; set; }
 
+    [DisplayName("Activo")]
     public int Active { get; set; }
 
     public virtual ICollection<Evento> Eventos { get; } = new List<Evento>();
